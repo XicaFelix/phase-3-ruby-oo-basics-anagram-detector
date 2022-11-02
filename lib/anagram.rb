e9c0af3884d7b8404eed616a338dfe2de145017b
Xica Felix <xicasfelix@gmail.com>
2022-11-02 10:52:53 -0400

    commit tree 84acf4afc8d344849fba91bec24852785312541f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    def initialize (word)
+        @word = word
+    end
+    def match array
+        @anagram = array.select do |ele|
+            ele.chars.sort == @word.chars.sort
+        end
+        @anagram
+    end
+end
